35dcf40694ff363b4e4f3375584c740f276868c6
GS-14 <rdm403@nyu.edu>
2018-02-25 11:01:18 -0500

    giving up

--- oauth/oauth2demo/oauth/__init__.py
+++
@@ -1,4 +0,0 @@
-from config.auth import AuthenticationParameters
-from core.oauthclient import DefaultSequencingOAuth2Client
-from core.filemetadata import DefaultSequencingFileMetadataApi
-
--- oauth/oauth2demo/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import include, url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.auth),
-    url(r'^sequencing/authorize', views.auth_callback),
-    url(r'^sequencing/authorization-approved', views.api, name="api"),
-]
